adaeb6e385d9700ea8418fa4e9957370bf74b22d
FA-Developes <the8flo@gmail.com>
2019-08-10 18:23:09 +0200

    1 file modified, 2 added

---
+++ Components/Auth.ts
@@ -0,0 +1,8 @@
+import {buildForm,XHR,apiXHR} from "../Utils";
+
+let login = buildForm(`<table>
+  <tr><th>Email:</th><td><input type="email" name="email" required></td></tr>
+  <tr><th>Password</th><td><input type="password" name="password" required></td></tr>
+  <tr><th></th><td><button type="submit">Login</button></td></tr>
+</table>`,(data) => {});
+
--- Utils.ts
+++ Utils.ts
@@ -1,12 +1,56 @@
 
+
+export class XHR {
+  constructor(public baseUrl: string, public headers = {"Content-Type": "application/json"}) {
+  }
+
+  request(method: string, url: string, body?: any) {
+    return new Promise((res, rej) => {
+      let xhr = new XMLHttpRequest();
+      for(let key in this.headers) {
+        xhr.setRequestHeader(key, this.headers[key])
+      }
+      xhr.open(method, this.baseUrl + url);
+      xhr.onload = () => xhr.status < 300? res(xhr.response) : rej(xhr.response);
+      xhr.onerror = (e) => rej(e);
+      if(body) {
+        xhr.send(JSON.stringify(body));
+      } else {
+        xhr.send();
+      }
+    })
+  }
+
+  get(url:string) {
+    return this.request("GET",url);
+  }
+
+  post(url:string, body: any) {
+    return this.request("GET",url, body);
+  }
+}
+
+export const apiXHR = new XHR("https://test-app-fa.herokuapp.com/")
 export type ObjectMap<T> = {[name: string]: T};
 
 export function htmlToElement(html: string): HTMLElement {
-  var template = document.createElement('template');
+  let template = document.createElement('template');
   template.innerHTML = html.trim(); 
   if(template.content) {
-    return <HTMLElement> template.content.firstChild;
+    return template.content.firstChild as HTMLElement;
   } else {
-    return <HTMLElement> template;
+    return template as HTMLElement;
+  }
+}
+
+export function buildForm(content: string, submitCb: (input:any) => void) {
+  let form = document.createElement('form');
+  form.innerHTML = content;
+  form.onsubmit = (e) => {
+    e.preventDefault();
+    let formdata = new FormData(form);
+    let obj = {};
+    formdata.forEach(((value, key) => obj[key] = value))
+
   }
 }
