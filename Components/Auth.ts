d88a731148ca6d52a178097aa1c43090b5cd40b5
FA-Developes <the8flo@gmail.com>
2019-08-10 20:37:00 +0200

    2 files modified

--- Components/Auth.ts
+++ Components/Auth.ts
@@ -1,9 +1,17 @@
 import { buildForm, XHR, apiXHR } from "../Utils";
+import {BehaviorSubject} from "rxjs";
 
+export class User {
+  name: string;
+  email: string;
+  isAdmin: boolean;
+}
+
+export const User$ = new BehaviorSubject<User>(null);
 let login = buildForm(`<table>
   <tr><th>Email:</th><td><input type="email" name="email" required></td></tr>
   <tr><th>Password</th><td><input type="password" name="password" required></td></tr>
-  <tr><th></th><td><button type="submit">Login</button></td></tr>
+  <tr><th></th><td><button type="submit">Login</but--ton></td></tr>
 </table>`,
   (data) => {
     apiXHR.post("auth/login", data)
@@ -22,7 +30,7 @@ function tryAuth() {
     if (expires > new Date()) {
       setTimeout(() => logout(), expires.getTime() - Date.now())
       apiXHR.headers["Authorization"] = "Bearer " + parsed.token;
-      apiXHR.get("auth/user").then(e => console.log(e)).catch(e => console.error(e));
+      apiXHR.get("auth/user").then((e: User) => User$.next(e)).catch(e => console.error(e));
     } else {
       logout()
     }
